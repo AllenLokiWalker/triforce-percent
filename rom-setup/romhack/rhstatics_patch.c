4531e32166e7f08bbcefa3471179cdea8fdd5e87
Sauraen <sauraen@gmail.com>
2021-01-09 00:34:48 -0800

    Trying to inject code to expansion pak

--- rom-setup/romhack/rhstatics_patch.c
+++ rom-setup/romhack/rhstatics_patch.c
@@ -20,16 +20,17 @@ typedef struct {
 
 // RAM: 0x800005A0, ROM: 0x000011A0
 void func_800005A0(void* arg0) {
-    static DmaRequest code1Info = { 0xDEADBEEF, &statics_START, 0x04206969 };
+    static DmaRequest rhstaticsFile = { 0xDEADBEEF, &statics_START, 0x04206969 };
     
     DmaMgr_Init();
     load_data_from_rom(_codeSegmentStart, _codeSegmentRomStart, _codeSegmentRomEnd - _codeSegmentRomStart, "");
     z_bzero(_codeSegmentBssStart, _codeSegmentBssEnd - _codeSegmentBssStart);
     /*
-    if (osGetMemSize() < 0x00800000) {
+    if (osGetMemSize() < 0x00800000) { 
         for (;;) {}
     }
     */
-    z_file_load(&code1Info);
+    z_file_load(&rhstaticsFile);
+    /*z_bzero(&rhstaticsFile, 12);*/
     Main(arg0);
 }
