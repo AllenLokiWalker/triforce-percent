8294af75298d5080e0ad54bc01ca0bf415cfcf11
Sauraen <sauraen@gmail.com>
2021-07-29 23:24:45 -0700

    Integrating ChamberOfSages

---
+++ scene/ChamberOfSages/ChamberOfSages_scene.h
@@ -0,0 +1,9 @@
+extern SCmdBase ChamberOfSages_scene_header00[];
+extern ActorEntry ChamberOfSages_scene_header00_startPositionList[];
+extern RomFile ChamberOfSages_scene_roomList[];
+extern EntranceEntry ChamberOfSages_scene_header00_entranceList[];
+extern LightSettings ChamberOfSages_scene_header00_lightSettings[1];
+extern u32 ChamberOfSages_polygonTypes[];
+extern CollisionPoly ChamberOfSages_polygons[];
+extern Vec3s ChamberOfSages_vertices[299];
+extern CollisionHeader ChamberOfSages_collisionHeader;
