a6fba208b368b1c30c61012fdee3928f72c2d645
Sauraen <sauraen@gmail.com>
2021-08-13 23:23:20 -0700

    Importing screen text

--- text/messages.c
+++ text/messages.c
@@ -20,6 +20,266 @@ DECLARE_MESSAGE(0x0902, BOX_BLACK, POS_MIDDLE,
 
 // 0xB00: Key of Courage
 
+DECLARE_MESSAGE(0x0B10, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Oh um. Are you looking for bugs," NEWLINE
+    "too? I love bugs! But I can't" NEWLINE
+    "find any. How's your luck?"
+)
+
+DECLARE_MESSAGE(0x0B11, BOX_BLACK, POS_MIDDLE,
+    "What is that? I asked you for a" NEWLINE
+    "bug!"
+)
+
+DECLARE_MESSAGE(0x0B12, BOX_BLACK, POS_MIDDLE,
+    "No, no! These are crawlies!" NEWLINE
+    "Crawlies are dumb. I want a flying" NEWLINE
+    "bug. You know, colorful, with wings." BOX_BREAK
+    "They like flowers, so you might find" NEWLINE
+    "one somewhere people grow lots of" NEWLINE
+    "different kinds of flowers."
+)
+
+DECLARE_MESSAGE(0x0B13, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Wow, you actually brought it! Thanks!" BOX_BREAK UNSKIPPABLE
+    "You're a nice guy, unlike that Mido..." NEWLINE
+    "He never talks to me. Except one time" NEWLINE
+    "he gave me this, and ordered me to" NEWLINE
+    "keep it safe." BOX_BREAK UNSKIPPABLE
+    "But I don't really want it... You can" NEWLINE
+    "have it!"
+)
+
+DECLARE_MESSAGE(0x0B14, BOX_BLUE, POS_MIDDLE,
+    UNSKIPPABLE ITEM_ICON("\x31") QUICKTEXT_ENABLE "You got the " COLOR(RED) "Magic Powder" COLOR(WHITE) "!" QUICKTEXT_DISABLE NEWLINE
+    "It smells like mushrooms."
+)
+
+DECLARE_MESSAGE(0x0B15, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "I don't think you can use it by" NEWLINE
+    "yourself, though."
+)
+
+DECLARE_MESSAGE(0x71B2, BOX_BLACK, POS_VARIABLE,
+    UNSKIPPABLE
+    "What's that? You're lookin' for a" NEWLINE
+    "flyin' bug?" BOX_BREAK UNSKIPPABLE
+    "I usually don' carry anything like" NEWLINE
+    "that, but my kid has been catchin'" NEWLINE
+    "a lot of 'em lately." BOX_BREAK UNSKIPPABLE
+    "He always leaves some around, and" NEWLINE
+    "I bet he'll be happy to know he" NEWLINE
+    "helped his old man make a sale!" NEWLINE
+    "Har har har!" BOX_BREAK UNSKIPPABLE
+    "Lemme put one out for ya."
+)
+
+DECLARE_MESSAGE(0x0010, BOX_BLUE, POS_MIDDLE,
+    UNSKIPPABLE ITEM_ICON("\x25") QUICKTEXT_ENABLE "You got a " COLOR(RED) "Butterfly" COLOR(WHITE) "!" QUICKTEXT_DISABLE NEWLINE
+    "Bug enthusiasts love these!"
+)
+
+DECLARE_MESSAGE(0x0B30, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Hey, Kokiri! I smell something" NEWLINE
+    "on you..." BOX_BREAK UNSKIPPABLE
+    "That smells like my Magic Powder." NEWLINE
+    QUICKTEXT_ENABLE "Give it back!" QUICKTEXT_DISABLE NEWLINE
+    TWO_CHOICE COLOR(GREEN) "Yes" NEWLINE
+    "No"
+)
+
+DECLARE_MESSAGE(0x0B31, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "You're as bad as that other" NEWLINE
+    "Kokiri kid!"
+)
+
+DECLARE_MESSAGE(0x0B32, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "I knew it, it's my Magic Powder!" BOX_BREAK UNSKIPPABLE
+    "That other nasty Kokiri kid stole" NEWLINE
+    "it from me. He said that because" NEWLINE
+    "it's made of forest mushrooms," NEWLINE
+    "it's his property." BOX_BREAK UNSKIPPABLE
+    "The nerve he had! Who made him" NEWLINE
+    "the owner of the forest?!" BOX_BREAK UNSKIPPABLE
+    "I plucked those mushrooms, dried" NEWLINE
+    "them, and mashed them myself!" BOX_BREAK UNSKIPPABLE
+    "grumble..." BOX_BREAK UNSKIPPABLE
+    "Anyway, it's not your fault." NEWLINE
+    "Thank you for bringing it back." BOX_BREAK UNSKIPPABLE
+    "As a reward, I'll use this powder" NEWLINE
+    "on your masks to make them more" NEWLINE
+    "powerful!"
+)
+
+DECLARE_MESSAGE(0x0013, BOX_BLUE, POS_MIDDLE,
+    UNSKIPPABLE ITEM_ICON("\x27") QUICKTEXT_ENABLE "You upgraded the " COLOR(RED) "Bunny Hood" COLOR(WHITE) "!" QUICKTEXT_DISABLE NEWLINE
+    "The magic in its long ears" NEWLINE
+    "allows the wearer to run" NEWLINE
+    "with great speed."
+)
+
+DECLARE_MESSAGE(0x0016, BOX_BLUE, POS_MIDDLE,
+    UNSKIPPABLE ITEM_ICON("\x2A") QUICKTEXT_ENABLE "You upgraded the " COLOR(RED) "Gerudo Mask" COLOR(WHITE) "!" QUICKTEXT_DISABLE NEWLINE
+    "Now you can charm people with" NEWLINE
+    "the perfect lady's disguise--" NEWLINE
+    "even Gerudo themselves!"
+)
+
+DECLARE_MESSAGE(0x0B40, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "A Gerudo child far from the desert?" NEWLINE
+    "My! Your father must be Hylian."
+)
+
+DECLARE_MESSAGE(0x0B41, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Is this your first time visiting?" NEWLINE
+    "Come in and greet your Gerudo" NEWLINE
+    "sisters!"
+)
+
+DECLARE_MESSAGE(0x0B50, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "So you come from outside? I bet" NEWLINE
+    "you had bountiful crops out there." BOX_BREAK UNSKIPPABLE
+    "Heh, here not even cacti bloom in" NEWLINE
+    "this desert."
+)
+
+DECLARE_MESSAGE(0x0B51, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Our fortress seems to be smaller" NEWLINE
+    "every day." BOX_BREAK UNSKIPPABLE
+    "Some day, I suppose there'll be" NEWLINE
+    "nothing left but desert."
+)
+
+DECLARE_MESSAGE(0x0B52, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "The Royal Family of Hyrule has" NEWLINE
+    "forsaken our people. They are" NEWLINE
+    "leaving us with no other option" NEWLINE
+    "than to fight!"
+)
+
+DECLARE_MESSAGE(0x0B53, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Oh, child. I pity you. Soon, you" NEWLINE
+    "will have to pick a side."
+)
+
+DECLARE_MESSAGE(0x0B60, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Hm? A kid?" NEWLINE
+    "What are you doing here?" BOX_BREAK UNSKIPPABLE
+    "Did your mother send you here to" NEWLINE
+    "meet the great Nabooru?" BOX_BREAK UNSKIPPABLE
+    "How nice of her. I'm pleased she" NEWLINE
+    "wishes her child to know of her" NEWLINE
+    "Gerudo origins." BOX_BREAK UNSKIPPABLE
+    "Very well! Guards, you may leave" NEWLINE
+    "the girl and I alone."
+)
+
+DECLARE_MESSAGE(0x0B61, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Well, enough acting, boy. Yes, I" NEWLINE
+    "can see through your disguise."
+)
+
+DECLARE_MESSAGE(0x0B62, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "A Hylian child pretending to be a" NEWLINE
+    "Gerudo. How will that help you in" NEWLINE
+    "in stopping the great Ganondorf?" BOX_BREAK UNSKIPPABLE
+    "That is what you want, isn't it?"
+)
+
+DECLARE_MESSAGE(0x0B63, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "You may think I'm your enemy," NEWLINE
+    "being a Gerudo like him." BOX_BREAK UNSKIPPABLE
+    "The truth is, that man is as much" NEWLINE
+    "against our kind as he is against" NEWLINE
+    "yours." BOX_BREAK UNSKIPPABLE
+    "The power of the Goddesses is all" NEWLINE
+    "he desires, even if that means" NEWLINE
+    "betraying us."
+)
+
+DECLARE_MESSAGE(0x0B64, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "But while your goal is admirable," NEWLINE
+    "you're still a child. No kid will" NEWLINE
+    "stop Ganondorf." BOX_BREAK UNSKIPPABLE
+    "An adult with your desire, however..." BOX_BREAK UNSKIPPABLE
+    "Ages ago, the Gerudo created a" NEWLINE
+    "melody known as the " COLOR(BLUE) "Song of Time" COLOR(WHITE) "." NEWLINE
+    "It is such powerful magic that it" NEWLINE
+    "was divided in two halves." BOX_BREAK UNSKIPPABLE
+    "The first half was granted to the" NEWLINE
+    "Royal Family, while the second" NEWLINE
+    "remained in this land."
+)
+
+//  "text text text text text text te"
+DECLARE_MESSAGE(0x0B65, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Do you know what lies beyond the" NEWLINE
+    "desert?" BOX_BREAK UNSKIPPABLE
+    "There's a place there known as the" NEWLINE
+    COLOR(TYPE1_ORANGE) "Desert Colossus" COLOR(WHITE) "." BOX_BREAK UNSKIPPABLE
+    "We Gerudo have used it as a temple" NEWLINE
+    "for generations, but it was built" NEWLINE
+    "by the ancient Sages." BOX_BREAK UNSKIPPABLE
+    "It is a two-sided dungeon made for" NEWLINE
+    "a time-traveling Hero who can go" NEWLINE
+    "from child to adult and back."
+)
+
+//  "text text text text text text te"
+DECLARE_MESSAGE(0x0B66, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "As I said, you're just a kid now." NEWLINE
+    "But I sense that you can control" NEWLINE
+    "even greater magic than just this" NEWLINE
+    "mask." BOX_BREAK UNSKIPPABLE
+    "If you're truly who I think you" NEWLINE
+    "are, I can teach you both halves" NEWLINE
+    "of the " COLOR(BLUE) "Song of Time" COLOR(WHITE) "." BOX_BREAK UNSKIPPABLE
+    "By playing it, you'll be able to" NEWLINE
+    "overcome the trials of the " COLOR(TYPE1_ORANGE) "Desert" NEWLINE
+    "Colossus" COLOR(WHITE) "," BOX_BREAK UNSKIPPABLE
+    "and you'll become the man who" NEWLINE
+    "could stand against Ganondorf." BOX_BREAK UNSKIPPABLE
+    "Is this really what you want?" NEWLINE
+    NEWLINE
+    TWO_CHOICE COLOR(GREEN) "Yes" NEWLINE
+    "NO"
+)
+
+DECLARE_MESSAGE(0x0B67, BOX_BLACK, POS_MIDDLE,
+    UNSKIPPABLE
+    "Come now, I know you want to stop" NEWLINE
+    "Ganondorf."
+)
+
+DECLARE_MESSAGE(0x00D5, BOX_BLUE, POS_MIDDLE,
+    UNSKIPPABLE
+    "You've learned the " COLOR(BLUE) "full Song of Time" COLOR(WHITE) "!" NEWLINE
+    "This song is proof of a true Hero's" NEWLINE
+    "courage." BOX_BREAK UNSKIPPABLE
+    "Play it as a child to become an adult," NEWLINE
+    "and as an adult to become a child."
+)
+
+
 // 0xC00: Key of Power
 
 // 0xD00: Key of Wisdom
@@ -140,7 +400,6 @@ DECLARE_MESSAGE(0x0A60, BOX_BLACK, POS_MIDDLE,
     "great destiny that is before you."
 )
 
-
 DECLARE_MESSAGE(0x0A70, BOX_BLACK, POS_MIDDLE,
     UNSKIPPABLE
     "Link, the great Hero of Time..." BOX_BREAK UNSKIPPABLE
@@ -166,14 +425,17 @@ DECLARE_MESSAGE(0x0A71, BOX_BLACK, POS_MIDDLE,
 
 DECLARE_MESSAGE(0x0A72, BOX_BLACK, POS_MIDDLE,
     UNSKIPPABLE
-    "Are you ready, Link?"
+    "Are you ready, Link?" NEWLINE
+    NEWLINE
+    TWO_CHOICE COLOR(GREEN) "Are you Zelda?" NEWLINE
+    "Are you a boy or a girl?"
 )
 
 //  "text text text text text text te"
 DECLARE_MESSAGE(0x0A73, BOX_BLACK, POS_MIDDLE,
     UNSKIPPABLE
     "I guess that hasn't been much of" NEWLINE
-    "a secret since " COLOR(LIGHTBLUE) "Melee" COLOR(WHITE) ", huh?" BOX_BREAK UNSKIPPABLE
+    "a secret since " COLOR(RED) "Melee" COLOR(WHITE) ", huh?" BOX_BREAK UNSKIPPABLE
     "Would be nice if I could get my" NEWLINE
     "own game someday..."
 )
@@ -227,7 +489,6 @@ DECLARE_MESSAGE(0x109D, BOX_NONE_BOTTOM, POS_VARIABLE,
     SHIFT("\x08") "and one wish to the Goddesses..." BOX_BREAK_DELAYED("\x28")
     SHIFT("\x02") "would you create something great?" FADE("\x50")
 )
-*/
 
 DECLARE_MESSAGE(0x109D, BOX_CREDITS, POS_VARIABLE,
     QUICKTEXT_ENABLE NEWLINE NEWLINE NEWLINE NEWLINE NEWLINE
@@ -248,7 +509,7 @@ DECLARE_MESSAGE(0x109D, BOX_CREDITS, POS_VARIABLE,
     QUICKTEXT_DISABLE BOX_BREAK_DELAYED("\x28")
     "dummy"
 )
-    
+*/
 
 //This one needs to come at the end
 DECLARE_MESSAGE(0xFFFF, BOX_BLUE, POS_MIDDLE,
